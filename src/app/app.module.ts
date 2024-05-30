75e9c67d8744188423605686795ae3138d9a5d00
KhoalinhNguyen <khoalinhnguyen123@gmail.com>
2024-05-30 20:45:04 +0200

    Update check out component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
   // path to match      when path matches, create new instance of component
@@ -44,7 +45,8 @@ const routes: Routes = [
     RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule,
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [ProductService], // service in provider wiil be able to be injected by other components
   bootstrap: [AppComponent]
