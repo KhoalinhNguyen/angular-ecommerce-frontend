e24a0ed34240e2a2a3531371180a03a1cf9caf7c
KhoalinhNguyen <khoalinhnguyen123@gmail.com>
2024-06-17 22:47:14 +0200

    Fix refresh page

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -14,7 +14,20 @@ export class CartService {
   totalPrice: Subject<number> = new BehaviorSubject<number>(0);
   totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
-  constructor() { }
+  storage: Storage = localStorage;
+
+  constructor() {
+
+      // read data from storage
+      let data = JSON.parse(this.storage.getItem('cartItems')!); // reads Json string and convert to object
+
+      if (data != null) {
+        this.cartItems = data;
+
+        // compute totals based on the data that is read from storage
+        this.computeCartTotals();
+      }
+   }
 
   addToCart(theCartItem: CartItem) {
 
@@ -78,7 +91,14 @@ export class CartService {
     this.totalQuantity.next(totalQuantityValue);
 
     // log cart data for debugging
-    this.logCartData(totalPriceValue, totalQuantityValue);
+    this.logCartData(totalPriceValue, totalQuantityValue); 
+
+    // persist the cart data
+    this.persistCartItems();
+  }
+
+  persistCartItems() {
+    this.storage.setItem('cartItems', JSON.stringify(this.cartItems)); // Json convert object to string
   }
 
   logCartData(totalPriceValue: number, totalQuantityValue: number) {
